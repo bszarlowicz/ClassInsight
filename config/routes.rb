393930f6ee6055453afe64496f2c46a2fc94c8ae
bszarlowicz <b.szarlowicz@gmail.com>
2024-02-18 21:55:48 +0100

    login and sign up - working procedure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :lessons
   devise_for :users
-  root "home#index"
+  root "lessons#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
