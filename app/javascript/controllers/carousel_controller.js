a3132190515bf8a7ffc087cd568940e84cbe0081
bszarlowicz <b.szarlowicz@gmail.com>
2024-10-23 00:03:47 +0200

    [new] login info carousel

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus";
+import * as bootstrap from 'bootstrap';
+
+export default class extends Controller {
+  connect() {
+    console.log('chuj')
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    const carouselElement = this.element.querySelector('#carouselAuto');
+
+    if (carouselElement) {
+      const bsCarousel = new bootstrap.Carousel(carouselElement, {
+        interval: 7000
+      });
+
+      bsCarousel.cycle();
+    }
+  }
+}
