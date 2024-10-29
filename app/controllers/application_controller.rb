99b3c1a4d605c0ad31c32c041752f905e75be6fe
bszarlowicz <b.szarlowicz@gmail.com>
2024-10-29 01:34:04 +0100

    [new] file structure and main content style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :show_left_menu?
+
+  def show_left_menu?
+    !(devise_controller? && (controller_name == "sessions" || controller_name == "registrations" || controller_name == "passwords"))
+  end
 end
