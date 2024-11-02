fa72abba05d1ccf4db294a7fbfedb771e533f8d2
bszarlowicz <b.szarlowicz@gmail.com>
2024-11-03 00:53:16 +0100

    [new] calendar navigation

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -6,6 +6,7 @@ import TuiDatePicker from "tui-date-picker";
 import TuiTimePicker from "tui-time-picker";
 
 export default class extends Controller {
+  static targets = ["currentMonth"];
   connect() {
     console.log('calendar here!')
     const daysOfWeek = JSON.parse(this.element.dataset.dayNames);
@@ -31,5 +32,41 @@ export default class extends Controller {
         }
       }
     });
+    this.updateMonthDisplay();
+  }
+
+  prev() {
+    this.calendar.prev();
+    this.updateMonthDisplay();
+  }
+
+  next() {
+    this.calendar.next();
+    this.updateMonthDisplay();
+  }
+
+  updateMonthDisplay() {
+    const startDate = this.calendar.getDateRangeStart().toDate();
+    const endDate = this.calendar.getDateRangeEnd().toDate();
+
+    const startMonth = this.capitalizeFirstLetter(startDate.toLocaleString('default', { month: 'long' }));
+    const startYear = startDate.getFullYear();
+    const endMonth = this.capitalizeFirstLetter(endDate.toLocaleString('default', { month: 'long' }));
+    const endYear = endDate.getFullYear();
+
+    let displayText;
+
+    if (startMonth === endMonth && startYear === endYear) {
+      displayText = `${startMonth} ${startYear}`;
+    } else if (startYear === endYear) {
+      displayText = `${startMonth}/${endMonth} ${startYear}`;
+    } else {
+      displayText = `${startMonth} ${startYear} / ${endMonth} ${endYear}`;
+    }
+    this.currentMonthTarget.textContent = displayText;
+  }
+
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 }
