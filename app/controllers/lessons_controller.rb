d8147168ec81e100bbeb8ccac288ca44749b6c84
bszarlowicz <b.szarlowicz@gmail.com>
2024-11-16 20:12:58 +0100

    [fix] modal form change

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -24,22 +24,17 @@ class LessonsController < ApplicationController
   # GET /lessons/1/edit
   def edit
     @selected_hour = @lesson.hour.present? ? @lesson.hour.strftime("%H:%M") : ""
-    @form_class = "main-form"
   end
 
   # POST /lessons or /lessons.json
   def create
     @lesson = @user.lessons.new(lesson_params)
-    @form_class = "main-form"
     respond_to do |format|
       if @lesson.save
         format.html { redirect_to user_schedule_path(current_user), notice: flash_message(:create, Lesson) }
         format.json { render :show, status: :created, location: @lesson }
       else
         format.json { render json: @lesson.errors, status: :unprocessable_entity }
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace('lesson_form', partial: 'lessons/form', locals: { lesson: @lesson })
-        end
       end
     end
   end
