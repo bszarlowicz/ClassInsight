f614094a5a7deb2ab72e1f7886c4873a2852ae36
bszarlowicz <b.szarlowicz@gmail.com>
2024-11-29 18:01:23 +0100

    [new] lesson teacher and user attachments

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,8 @@ class Lesson < ApplicationRecord
   belongs_to :teacher, class_name: 'Teacher', foreign_key: 'teacher_id'
   belongs_to :student, class_name: 'Student', foreign_key: 'student_id'
   has_many :topics, dependent: :destroy
+  has_many_attached :teacher_files
+  has_many_attached :student_files
   
   after_initialize :set_arrays
   before_save :set_all_occurrences
