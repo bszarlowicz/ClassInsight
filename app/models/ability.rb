43ff15364de8eb276c5122e6230f0a060dfdd050
bszarlowicz <b.szarlowicz@gmail.com>
2024-11-30 16:48:42 +0100

    [new] ability config

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,13 @@ class Ability
 
   def initialize(user)
     if user.is_a?(Student)
-
+      can [:index], Lesson
+      can [:index, :edit, :update], Lesson, student_id: user.id
     end
 
     if user.is_a?(Teacher)
+      can [:index, :new, :create], Lesson
+      can [:edit, :update, :destroy], Lesson, teacher_id: user.id
       can :manage, Topic
     end
 
